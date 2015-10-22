1f46d7e79d129999d5e85a992e0f9f22993af7fd
ReedGit <350099287@qq.com>
2015-10-22 10:21:14 +0800

    导入volley库

--- app/src/main/java/com/giot/meeting/LoginActivity.java
+++ app/src/main/java/com/giot/meeting/LoginActivity.java
@@ -54,6 +54,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+
     @Override
     public void onClick(View view) {
         Intent intent;
---
+++ app/src/main/java/com/giot/meeting/configs/SysConstants.java
@@ -0,0 +1,11 @@
+package com.giot.meeting.configs;
+
+
+/**
+ * Created by 伟 on 2015/10/22.
+ */
+public class SysConstants {
+
+    public final static String BaseUrl = "http://192.168.1.102:8080/whentomeet";
+
+}
